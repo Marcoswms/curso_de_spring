b3a030a3882d7e94a7db2c58c355419936d3dae5
Marcos Willian <marcoswms@gmail.com>
2025-03-10 03:10:48 -0300

    Add projeto de estudo Java Spring

---
+++ src/main/java/com/kipper/firs_spring_app/FirsSpringAppApplication.java
@@ -0,0 +1,17 @@
+package com.kipper.firs_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //Anotação que define a porta de entrada do Spring boot e que concentrar as 3 anotações abaixo:
+//@Configuration - Permite métodos que define 'Bins'.
+//@EnableAutoConfiguration - Classe que vai ativar a autoconfiguração do SpringBoot
+//@ComponentScan - Permite que o Spring escaneie os pacote do nosso projeto em busca das classes de configuração,
+//                 componentes, classes de service e controladores.
+public class FirsSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirsSpringAppApplication.class, args);
+    }
+
+}
